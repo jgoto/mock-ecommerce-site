74de1f0d657b6338a40e0a26dd009314eeb4d169
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2025-06-29 22:21:03 -0400

    Added css for the Store page

--- src/components/ProductEntry.jsx
+++ src/components/ProductEntry.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
+import styles from './ProductEntry.module.css'
 
 function ProductEntry({product}){
     return (
-        <div id={product.id}>
-            <h3>{product.name}</h3>
-            <ul>
-                <li>{product.description}</li>
-                <li>{product.dimensions}</li>
-                <li>{product.weight}</li>
-                <li>${product.price}</li>
-            </ul>
+        <div id={product.id} className={styles.productContainer}>
             <img src={product.image} alt={product.name} />
+            <div className={styles.productDetails}>
+                <h3>{product.name}</h3>
+                <ul>
+                    <li>{product.description}</li>
+                    <li>{product.dimensions}</li>
+                    <li>{product.weight}</li>
+                    <li>${product.price}</li>
+                </ul>    
+            </div>
         </div>
     )
 }
