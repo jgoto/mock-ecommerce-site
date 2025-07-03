f86add4dd781cb3e8a630e93f28fe91452f81f30
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2025-07-02 23:36:50 -0400

    Another fix to the image sources to address broken links in deployment

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,10 +5,10 @@ function Socials(){
     return (
         <div>
             <ul className={styles.socialsList}>
-                <li><a href="#" target="_blank"><img src="/public/assets/icons/logo-black.png"  className = {styles.socialIcon} alt = "X" rel="noopener noreferrer" /></a></li>
-                <li><a href="#" target="_blank"><img src="/public/assets/icons/facebook_logo.png" className = {styles.socialIcon} alt="Facebook" rel="noopener noreferrer" /></a></li>
-                <li><a href="#" target="_blank"><img src="/public/assets/icons/instagram-logo.png" className = {styles.socialIcon} alt = "Instagram" rel="noopener noreferrer" /></a></li>
-                <li><a href="https://github.com/jgoto" target="_blank"><img src="/public/assets/icons/github-mark.png" className = {styles.socialIcon} alt = "GitHub" rel = "noopener noreferrer" /></a></li>
+                <li><a href="#" target="_blank"><img src="/assets/icons/logo-black.png"  className = {styles.socialIcon} alt = "X" rel="noopener noreferrer" /></a></li>
+                <li><a href="#" target="_blank"><img src="/assets/icons/facebook_logo.png" className = {styles.socialIcon} alt="Facebook" rel="noopener noreferrer" /></a></li>
+                <li><a href="#" target="_blank"><img src="/assets/icons/instagram-logo.png" className = {styles.socialIcon} alt = "Instagram" rel="noopener noreferrer" /></a></li>
+                <li><a href="https://github.com/jgoto" target="_blank"><img src="/assets/icons/github-mark.png" className = {styles.socialIcon} alt = "GitHub" rel = "noopener noreferrer" /></a></li>
             </ul>
         </div>
     )
